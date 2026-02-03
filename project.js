e9ead5697a82df9741ec98f80576b21cb0b38ec3
Salman2293334 <salmaanhaashim13@gmail.com>
2026-02-03 15:16:48 +0300

    Add files via upload

---
+++ project.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("expenseForm");
+const expenseList = document.getElementById("expenseList");
+const totalAmount = document.getElementById("totalAmount");
+const errorMessage = document.getElementById("errorMessage");
+
+let total = 0;
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("expenseName").value;
+  const amount = document.getElementById("expenseAmount").value;
+
+  
+  if (name === "" || amount === "") {
+    errorMessage.textContent = " All fields are required";
+    return;
+  }
+
+  if (amount <=-1) {
+    errorMessage.textContent = " Amount must be greater than zero";
+    return;
+  }
+
+  errorMessage.textContent = "";
+
+  const li = document.createElement("li");
+  li.textContent = `${name} - $${amount}`;
+  expenseList.appendChild(li);
+
+  total += Number(amount);
+  totalAmount.textContent = total;
+
+  form.reset();
+});
